e1a123b5cd0a4246345ff6ffed58f2e42786e8a7
Cristy Gonzales <cristyg2025@gmail.com>
2022-04-09 11:49:59 -0700

    Implemented link validation

--- src/services/linkgroup/linkgroup.hooks.js
+++ src/services/linkgroup/linkgroup.hooks.js
@@ -6,13 +6,65 @@ const generateSlug = () => (context) => {
 
   return context;
 };
+const validateFields = () => (context) => {
+  if (!Array.isArray(context.data.links)) {
+    throw new Error("Links must be an array.");
+  }
+
+  const isLinksValidObject = context.data.links.every((element) => {
+    if (
+      typeof element === "object" &&
+      !Array.isArray(element) &&
+      element !== null
+    ) {
+      return ["name", "link"].every((key) => element.hasOwnProperty(key));
+    }
+    return false;
+  });
+  if (!isLinksValidObject) {
+    throw new Error("Each link must have a name and link field.");
+  }
+
+  const isLinksValid = context.data.links.every((e) => {
+    try {
+      return new URL(e.link);
+    } catch (_) {
+      return false;
+    }
+  });
+  if (!isLinksValid) {
+    throw new Error("Each link must be a valid URL.")
+  }
+  return context;
+};
+
+const maxLinks = () => (context) => {
+  if (context.data.links.length <= 20) {
+    return context;
+  }
+  throw new Error("Cannot have more than 20 links.");
+};
+
+const linkTitle = () => (context) => {
+  context.data.links = context.data.links.map((e) => {
+    if (!e.name) {
+      const url = new URL(e.link);
+      return {
+        ...e,
+        name: url.hostname.replace(/.+\/\/|www.|\..+/g, ""),
+      };
+    }
+    return e;
+  });
+  return context;
+};
 
 module.exports = {
   before: {
     all: [],
     find: [],
     get: [],
-    create: [generateSlug()],
+    create: [validateFields(), maxLinks(), generateSlug(), linkTitle()],
     update: [],
     patch: [],
     remove: [],
